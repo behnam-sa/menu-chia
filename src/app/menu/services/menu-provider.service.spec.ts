0eca5cbfddc5e72b11fb214335dfee475364a0b3
= <saedi.behnam.336@gmail.com>
2023-06-25 17:28:28 +0330

    Fix tests

--- src/app/menu/services/menu-provider.service.spec.ts
+++ src/app/menu/services/menu-provider.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MenuProviderService } from './menu-provider.service';
 
 describe('MenuProviderService', () => {
     let service: MenuProviderService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+        });
         service = TestBed.inject(MenuProviderService);
     });
 
