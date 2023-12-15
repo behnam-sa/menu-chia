68743847e0f9238393ebf44291f212f802ec3829
= <saedi.behnam.336@gmail.com>
2023-12-16 00:58:17 +0330

    Remove unused MatModules

--- src/material/material.module.ts
+++ src/material/material.module.ts
@@ -3,9 +3,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -15,11 +13,9 @@ const matModules = [
     LayoutModule,
     MatIconModule,
     MatToolbarModule,
-    MatExpansionModule,
     MatCardModule,
     MatSidenavModule,
     MatButtonModule,
-    MatListModule,
     MatDialogModule,
     MatProgressSpinnerModule,
     MatProgressBarModule,
